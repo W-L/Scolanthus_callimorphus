db4838fcc836c62ba8ff26b213d8a95fc2d9b232
W-L <lukas.weilguny@24speed.at>
2019-04-05 16:59:42 +0200

    generalized script

--- BUSCOplot.R
+++ BUSCOplot.R
@@ -1,5 +1,4 @@
-# plot BUSCO results for three assemblies
-
+# plot BUSCO results 
 
 library(ggplot2)
 library(data.table)
@@ -7,46 +6,59 @@ library(plyr)
 library(forcats)
 library(ggrepel)
 
-setwd("/home/lukas/cube_home/02_BUSCO/")
 
-# read in the Busco output directly
-canu <- fread(cmd='grep -v "#" run_busco_canu/short_summary_busco_canu.txt')
-falcon <- fread(cmd='grep -v "#" run_busco_falcon/short_summary_busco_falcon.txt')
-miniasm <- fread(cmd='grep -v "#" run_busco_miniasm2/short_summary_busco_miniasm2.txt')
+BUSCOtable <- function(path, name){
+  # load data
+  data <- fread(cmd=paste('grep -v "#"', path))
+  
+  # add col for busco run name
+  data$name <- rep(name, length(data$V2))
+  
+  # drop first col
+  data <- data[,-1]
+  
+  # rename factors
+  data$recat <- revalue(data$V3, replace=c("Complete BUSCOs (C)"="complete", 
+                                           "Complete and single-copy BUSCOs (S)"="complete (single-copy)",
+                                           "Complete and duplicated BUSCOs (D)"="complete (duplicated)",
+                                           "Fragmented BUSCOs (F)"="fragmented",
+                                           "Missing BUSCOs (M)"="missing",
+                                           "Total BUSCO groups searched"="total"))
+  
+  # exclude total and comp
+  data <- subset(data, !(recat %in% c('total', 'complete')))
+  
+  # as factor, explicit order
+  data$recat <- factor(data$recat, levels = rev(c("complete (single-copy)",
+                                                              "complete (duplicated)",
+                                                              "fragmented",
+                                                              "missing")))
+  return(data)
+}
 
-# add a col for the assembler
-canu$assembler <- rep('canu', length(canu$V2))
-falcon$assembler <- rep('falcon', length(falcon$V2))
-miniasm$assembler <- rep('miniasm', length(miniasm$V2))
 
-# merge the asemblies
-assemblies <- rbind(canu, falcon, miniasm)
+canu <- BUSCOtable(path = '/home/lukas/cube_scratch/02_BUSCO/run_busco_canu/short_summary_busco_canu.txt',
+                  name = 'canu')
 
-# rename the factors
-assemblies$recat <- revalue(assemblies$V3, replace=c("Complete BUSCOs (C)"="complete", 
-                                          "Complete and single-copy BUSCOs (S)"="complete (single-copy)",
-                                          "Complete and duplicated BUSCOs (D)"="complete (duplicated)",
-                                          "Fragmented BUSCOs (F)"="fragmented",
-                                          "Missing BUSCOs (M)"="missing",
-                                          "Total BUSCO groups searched"="total"))
+falcon <- BUSCOtable(path = '/home/lukas/cube_scratch/02_BUSCO/run_busco_falcon/short_summary_busco_falcon.txt',
+                   name = 'falcon')
 
-# drop the first empty column
-assemblies <- assemblies[,-1]
+miniasm <- BUSCOtable(path = '/home/lukas/cube_scratch/02_BUSCO/run_busco_miniasm2/short_summary_busco_miniasm2.txt',
+                   name = 'miniasm racon')
 
-# exclude the total number and complete
-assemblies <- subset(assemblies, !(recat %in% c('total', 'complete')))
+canu_augustus <- BUSCOtable(path = '/home/lukas/cube_scratch/11_BUSCO_proteins/run_busco_sc1_canu_augustus/short_summary_busco_sc1_canu_augustus.txt',
+                            name = 'canu_augustus_proteins')
 
-assemblies$recat <- factor(assemblies$recat, levels = rev(c("complete (single-copy)",
-                                                        "complete (duplicated)",
-                                                        "fragmented",
-                                                        "missing")))
+# merge the BUSCOs
+BUSCO <- rbind(canu, falcon, miniasm, canu_augustus)
+
 
 # make some labels - calc y pos and center - painful
-assemblies <- plyr::arrange(assemblies, assembler, forcats::fct_rev(recat))
-assemblies <- ddply(assemblies, "assembler", transform, label_y=cumsum(V2) - 0.5*V2)
+BUSCO <- plyr::arrange(BUSCO, name, forcats::fct_rev(recat))
+BUSCO <- ddply(BUSCO, "name", transform, label_y=cumsum(V2) - 0.5*V2)
 
 # stacked bar plot
-p <- ggplot(data=assemblies, aes(y=V2, x=assembler, fill=recat)) + 
+p <- ggplot(data=BUSCO, aes(y=V2, x=name, fill=recat)) + 
   geom_bar(stat = 'identity') +
   scale_fill_brewer() +
   geom_text_repel(aes(y=label_y, label=paste(round(V2/978*100,2), '%')), size=2.5, direction='y', alpha=0.6) +
@@ -56,8 +68,9 @@ p <- ggplot(data=assemblies, aes(y=V2, x=assembler, fill=recat)) +
   ylab("genes") +
   xlab('') +
   theme_minimal()
-ggsave(p, file="~/cube_home/02_BUSCO/BUSCO_scores.pdf", width=7,height=3)
+p
+#ggsave(p, file="~/cube_home/02_BUSCO/BUSCO_scores.pdf", width=7,height=3)
 
 
 
-  
+
